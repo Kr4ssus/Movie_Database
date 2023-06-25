69be96ea25ec42c61ccd8de629079bfddc8621c2
kr4ssus <tom32183218@gmail.com>
2023-06-25 19:54:43 +0100

    Add project files.

---
+++ Movie_Database/Data/Movie_DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movie_Database.Models;
+
+namespace Movie_Database.Data
+{
+    public class Movie_DatabaseContext : DbContext
+    {
+        public Movie_DatabaseContext (DbContextOptions<Movie_DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie_Database.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Movie_Database/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Movie_Database.Data;
+using Movie_Database.Models;
+
+namespace Movie_Database.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Movie_Database.Data.Movie_DatabaseContext _context;
+
+        public CreateModel(Movie_Database.Data.Movie_DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

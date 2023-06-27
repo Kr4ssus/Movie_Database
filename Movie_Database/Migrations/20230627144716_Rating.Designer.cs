a3b76de17aeb9e93ca4af297619726b728e23ab4
Thomas Ackland-Jones <Thomas.Ackland-Jones@sgs.com>
2023-06-27 16:28:05 +0100

    Added validation

--- Movie_Database/Migrations/20230627144716_Rating.Designer.cs
+++ Movie_Database/Migrations/20230627144716_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movie_Database.Data;
 
@@ -11,9 +12,11 @@ using Movie_Database.Data;
 namespace Movie_Database.Migrations
 {
     [DbContext(typeof(Movie_DatabaseContext))]
-    partial class Movie_DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230627144716_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Movie_Database/Models/Movie.cs
+++ Movie_Database/Models/Movie.cs
@@ -7,12 +7,15 @@ namespace Movie_Database.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; } = string.Empty;
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
+        public string? Genre { get; set; } = string.Empty;
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; } = string.Empty;
     }
 }
